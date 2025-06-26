187eaef08dac4850bae28e0842db77d6b6119eaa
Mukesh Kumar Debata <49590845+mukeshdebata@users.noreply.github.com>
2025-06-26 06:49:33 +0000

    02-exp-tracking new

--- 02-Experiment-Tracking/register_model.py
+++ 02-Experiment-Tracking/register_model.py
@@ -13,33 +13,23 @@ EXPERIMENT_NAME = "random-forest-best-models"
 RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state']
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
 
 def load_pickle(filename):
-    """Load a pickle file"""
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
 
 
-def restore_experiment(client, experiment_name):
-    """Restore the experiment if it is in a 'deleted' state"""
-    experiment = client.get_experiment_by_name(experiment_name)
-    if experiment and experiment.lifecycle_stage == "deleted":
-        client.restore_experiment(experiment.experiment_id)
-        print(f"Experiment '{experiment_name}' has been restored.")
-    return experiment
-
-
 def train_and_log_model(data_path, params):
-    """Train a RandomForest model and log the training details"""
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
     X_test, y_test = load_pickle(os.path.join(data_path, "test.pkl"))
 
     with mlflow.start_run():
         for param in RF_PARAMS:
-            params[param] = int(params[param])  # Convert string params to integers where applicable
+            params[param] = int(params[param])
 
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
@@ -64,35 +54,24 @@ def train_and_log_model(data_path, params):
     help="Number of top models that need to be evaluated to decide which one to promote"
 )
 def run_register_model(data_path: str, top_n: int):
-    """Retrieve, evaluate, and register the best model"""
 
     client = MlflowClient()
 
-    # Restore experiments if they are deleted
-    hpo_experiment = restore_experiment(client, HPO_EXPERIMENT_NAME)
-    if hpo_experiment is None:
-        raise ValueError(f"Experiment '{HPO_EXPERIMENT_NAME}' does not exist.")
-
-    best_models_experiment = restore_experiment(client, EXPERIMENT_NAME)
-    if best_models_experiment is None:
-        # Create the experiment if it does not exist
-        experiment_id = client.create_experiment(EXPERIMENT_NAME)
-        print(f"Experiment '{EXPERIMENT_NAME}' has been created with ID {experiment_id}.")
-
     # Retrieve the top_n model runs and log the models
+    experiment = client.get_experiment_by_name(HPO_EXPERIMENT_NAME)
     runs = client.search_runs(
-        experiment_ids=hpo_experiment.experiment_id,
+        experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
         order_by=["metrics.rmse ASC"]
     )
     for run in runs:
-        print(f"Training model with parameters from run {run.info.run_id}")
         train_and_log_model(data_path=data_path, params=run.data.params)
 
     # Select the model with the lowest test RMSE
+    experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     best_run = client.search_runs(
-        experiment_ids=best_models_experiment.experiment_id,
+        experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
         order_by=["metrics.test_rmse ASC"]
@@ -101,10 +80,7 @@ def run_register_model(data_path: str, top_n: int):
     # Register the best model
     run_id = best_run.info.run_id
     model_uri = f"runs:/{run_id}/model"
-    registered_model_name = "rf-best-model"
-
-    print(f"Registering model from run {run_id} to model registry with name '{registered_model_name}'")
-    mlflow.register_model(model_uri, name=registered_model_name)
+    mlflow.register_model(model_uri, name="rf-best-model")
 
 
 if __name__ == '__main__':
